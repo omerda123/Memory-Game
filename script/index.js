176e43774014dab6a0934639b071921d124138e7
Omerda <omerda@gmail.com>
2019-12-26 17:59:06 +0200

    create random pictures for each board size

--- script/index.js
+++ script/index.js
@@ -1,5 +1,4 @@
 
-const container = document.querySelector(".container");
 
 
 class Board {
@@ -10,6 +9,7 @@ class Board {
     clearBoard() {
         const divs = document.querySelectorAll(".container div");
         divs.forEach(div => div.remove());
+        array2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     }
 
     createBoard(rows, cols) {
@@ -21,27 +21,58 @@ class Board {
                 let col = document.createElement("div");
                 col.className = "col"
                 row.appendChild(col);
+                let pic = document.createElement("img");
+                pic.src = `./images/pokemon/${randomPicNumberPairs(rows, cols)}.png`;
+                col.appendChild(pic);
             }
         }
     }
+
+
+    createBoardByDifficulty(e) {
+        switch (e.target.value) {
+            case "easy": gameBoard.clearBoard(); gameBoard.createBoard(3, 4);
+                break;
+            case "medium":gameBoard.clearBoard();gameBoard.createBoard(4, 5);
+                break;
+            case "hard": gameBoard.clearBoard(); gameBoard.createBoard(4, 6);
+                break;
+            default: gameBoard.clearBoard(); gameBoard.createBoard(3, 4);
+                break;
+        }
+    }
+}
+const randomPicNumberPairs = (rows, cols) => {
+    let numOfPics = rows * cols /2  ;
+    console.log(numOfPics)
+    const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let y = parseInt(Math.random() * (numOfPics) );
+    if (array2[y] === 0) {
+
+        array2[y]++;
+        return array1[y];
+    }
+    else if (array2[y] === 1) {
+        array2[y]++;
+        console.log(array2);
+        console.log(array1[y])
+        return array1[y];
+    }
+    else if (array2[y] === 2) {
+        return randomPicNumberPairs(rows, cols);
+    }
+    else {
+        console.log("error")
+    }
+
+
+
 }
 
+
+let array2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const gameBoard = new Board;
-gameBoard.createBoard(3, 4);
-
-
 const difficulty = document.querySelectorAll(".difficulty");
-difficulty.forEach(item => item.addEventListener("click", (e) => {
-    console.log(e.target.value);
-    switch (e.target.value) {
-        case "easy": gameBoard.clearBoard(); gameBoard.createBoard(3, 4);
-            break;
-        case "medium": gameBoard.clearBoard(); gameBoard.createBoard(4, 5);
-            break;
-        case "hard": gameBoard.clearBoard(); gameBoard.createBoard(4, 6);
-            break;
-        default: gameBoard.clearBoard(); gameBoard.createBoard(3, 4);
-            break;
-    }
-}))
-console.log(difficulty);
+const container = document.querySelector(".container");
+difficulty.forEach(item => item.addEventListener("click", gameBoard.createBoardByDifficulty));
+gameBoard.createBoard(3, 4);
